e652dd2a388e1c666dceec87b8a6b5bf6f4ec90f
SYASXYT <129880047+SYASXYT@users.noreply.github.com>
2023-04-10 21:15:16 -0400

    Add files via upload

---
+++ Leak/Oct 2020/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: AssemblyProduct("WeAreDevs_API.Properties")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6d8a8bf5-f458-403a-a65f-8989c36536fd")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
